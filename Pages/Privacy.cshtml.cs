01b2ef7ce0427718127d500b7485dc402b0367a5
James High <jhigh2000@gmail.com>
2023-11-16 11:07:58 -0600

    HTTP/2 timeout minimal repro

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        // set a breakpoint here and wait to simulate a long debug session
+        var s0 = 0;
+    }
+}
